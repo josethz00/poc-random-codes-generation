523b9e33bc1769ed7b214116785aaf62756f8919
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-09-06 18:15:11 -0300

    feat: remove batch_size

--- generate_codes.py
+++ generate_codes.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generate_code(batch_size=1):
+def generate_code():
     number_part = f"{random.randint(0, 9999):04}"
     
     letters_part = ''.join(random.choices(string.ascii_uppercase, k=3))
