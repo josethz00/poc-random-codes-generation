55724f4739d9a2667cbc56ec76029ee79ba26982
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-09-07 20:48:41 -0300

    feat: pg pool and multithreading

--- generate-and-copy-batch.py
+++ generate-and-copy-batch.py
@@ -1,15 +1,23 @@
-import time
-import threading
-import psycopg2
-import openpyxl
-import redis
+import string
 from generate_codes import generate_code
+import psycopg2.pool
+import redis
+import openpyxl
+import subprocess
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import itertools
+import logging
+import time
 
-pgconn = psycopg2.connect(database="codes-db",
-                          host="localhost",
-                          user="codes-db",
-                          password="codes-db",
-                          port="6492")
+# Create the connection pool
+db_pool = psycopg2.pool.SimpleConnectionPool(1, 5,
+                                             database="codes-db",
+                                             host="localhost",
+                                             user="codes-db",
+                                             password="codes-db",
+                                             port="6492")
+
 redisconn = redis.Redis(host="localhost", port=6479, db=0)
 
 desired_codes_amount = int(input("How many codes do you want to generate?  "))
@@ -33,16 +41,21 @@ wb = openpyxl.Workbook()
 ws = wb.active
 
 def save_xlsx():
+    logging.info("Saving xlsx file...")
     wb.save(filename_xlsx)
-    print("Saved xlsx file")
+    logging.info("Saved xlsx file")
 
+# Using connection pool for PostgreSQL
+pgconn = db_pool.getconn()
 cursor = pgconn.cursor()
 cursor.execute("SELECT code FROM codes")
 codes_from_db = {row[0] for row in cursor.fetchall()}
+cursor.close()
+db_pool.putconn(pgconn)
+
 if codes_from_db:
     redisconn.sadd("codes", *codes_from_db)
     existing_codes.update(codes_from_db)
-cursor.close()
 
 with open(filename, "w") as f:
     for i in range(desired_codes_amount):
@@ -72,12 +85,31 @@ with open(filename, "w") as f:
         flush_to_file(f, file_buffer)
 
 save_xlsx_thread = threading.Thread(target=save_xlsx)
+save_xlsx_thread.start()
 
-cursor = pgconn.cursor()
-with open(filename, "r") as f:
-    cursor.copy_from(f, 'codes', columns=('code',))
-pgconn.commit()
+subprocess.run(["split", "-l", str(desired_codes_amount // 5), "codes_to_insert.txt", "outcodes_"])
+
+def copy_to_db(filename):
+    local_conn = db_pool.getconn()
+    try:
+        with local_conn.cursor() as local_cursor:
+            with open(filename, "r") as f:
+                local_cursor.copy_from(f, 'codes', columns=('code',))
+            local_conn.commit()
+    finally:
+        db_pool.putconn(local_conn)
+
+thread_pool = ThreadPoolExecutor(max_workers=5)
+file_prefixes = [''.join(i) for i in itertools.product('a', string.ascii_lowercase)][:5]
+for prefix in file_prefixes:
+    thread_pool.submit(copy_to_db, f"outcodes_{prefix}")
+
+thread_pool.shutdown(wait=True)
+save_xlsx_thread.join()
 
 end_time = time.time()
 
 print(f"Time elapsed: {end_time - start_time} seconds")
+
+# Close the pool when done
+db_pool.closeall()
