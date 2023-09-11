9beacf2e71e965af7148edb0efe8f00d7d66b7fa
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-09-11 14:35:54 -0300

    feat: use sets instead of arrays

--- javascript/main.ts
+++ javascript/main.ts
@@ -1,6 +1,10 @@
-import { BunFile } from 'bun';
-import pg from 'pg';
-import redis from 'redis';
+import * as fs from 'fs';
+import * as pg from 'pg';
+import * as redis from 'redis';
+import path from 'path';
+import readline from 'readline';
+import { spawn } from 'child_process';
+import { generateCode } from './generate-code';
 
 const dbPool = new pg.Pool({
     host: 'localhost',
@@ -15,39 +19,96 @@ const redisConn = redis.createClient({
     url: 'redis://localhost:6479'
 });
 
-const desiredCodesAmount: number = console.write('How many codes do you want to generate?   ');
-const startTime: number = Date.now();
-const existingCodes: string[] = await redisConn.sMembers('codes');
-const BATCH_SIZE = 1600;
-const codesBatchSet: Set<string> = new Set();
-const codesBatchArray: string[] = [];
-const filenameCsv = "codes_to_insert.csv";
+const main = async () => {
+    await redisConn.connect();
 
-function flushToCsvFile(fileObj: BunFile, buffer: string[]) {
-    const writer = fileObj.writer();
-    writer.write(buffer.join('\n'));
-    writer.flush();
-    writer.end();
-    buffer.length = 0;
-}
-
-const dbConn = await dbPool.connect();
-const codesFromDb: string[] = await dbConn.query('SELECT * FROM codes')
-    .then(res => res.rows.map(row => row.code));
-dbConn.release();
-
-if (codesFromDb.length > 0) {
-    codesFromDb.forEach(code => {
-        redisConn.sAdd('codes', code);
-        existingCodes.push(code)
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
-}
 
-const csvFile = Bun.file(filenameCsv);
+    const desiredCodesAmount = await new Promise<number>(resolve => {
+        rl.question('How many codes do you want to generate?   ', answer => {
+            rl.close();
+            resolve(Number(answer));
+        });
+    });
 
-for (let i = 0; i < desiredCodesAmount; i++) {
-    let code = '';
-    do {
-        code = generateCode();
-    } while (existingCodes.includes(code) || codesBatchSet.has(code));
-}
+    const startTime: number = Date.now();
+    const existingCodes: Set<string> = new Set(await redisConn.sMembers('codes'));
+    const BATCH_SIZE = 1600;
+    const codesBatchSet: Set<string> = new Set();
+    const codesBatchArray: string[] = [];
+    const filenameCsv = "codes_to_insert.csv";
+    const csvFileBuffer: string[] = [];
+
+    function flushToCsvFile(fileObj: string, buffer: string[]): void {
+        fs.appendFileSync(fileObj, buffer.join('\n'));
+        buffer.length = 0;
+    }
+
+    const dbConn = await dbPool.connect();
+    const codesFromDb: string[] = await dbConn.query('SELECT * FROM codes')
+        .then(res => res.rows.map(row => row.code));
+    dbConn.release();
+
+    if (codesFromDb.length > 0) {
+        codesFromDb.forEach(code => {
+            redisConn.sAdd('codes', code);
+            existingCodes.add(code)
+        });
+    }
+
+    for (let i = 0; i < desiredCodesAmount; i++) {
+        let code = '';
+        do {
+            code = generateCode();
+        } while (existingCodes.has(code) || codesBatchSet.has(code));
+
+        codesBatchSet.add(code);
+        codesBatchArray.push(code);
+        existingCodes.add(code);
+        csvFileBuffer.push("," + code + '\n');
+
+        if (csvFileBuffer.length >= BATCH_SIZE) {
+            flushToCsvFile(filenameCsv, csvFileBuffer);
+            redisConn.sAdd('codes', codesBatchArray);
+            codesBatchSet.clear();
+            codesBatchArray.length = 0;
+        }
+
+        if (i % 10000 === 0) {
+            console.log(`Generated ${i} codes so far... `);
+        }
+    }
+
+    if (codesBatchSet.size > 0) {
+        redisConn.sAdd('codes', codesBatchArray);
+        codesBatchSet.clear();
+        codesBatchArray.length = 0;
+    }
+
+    if (csvFileBuffer.length > 0) {
+        flushToCsvFile(filenameCsv, csvFileBuffer);
+    }
+
+    spawn('split', ['-l', String(Math.floor(desiredCodesAmount / 5)), filenameCsv, 'outcodes_']);
+
+    async function copyToDb(filename: string) {
+        const dbConn = await dbPool.connect();
+        dbConn.copyFrom(`COPY codes FROM '${filename}' DELIMITER ',' CSV HEADER`);
+        dbConn.release();
+    }
+
+    const filePrefixes = [...Array(5).keys()].map(num => String.fromCharCode(97 + num));  // ['a', 'b', 'c', 'd', 'e']
+    filePrefixes.forEach(prefix => {
+        const worker = new Worker(path.join(__dirname, 'worker.ts'));
+        worker.postMessage(`outcodes_${prefix}`);
+    });
+
+    const endTime: number = Date.now();
+
+    console.log(`Time elapsed: ${(endTime - startTime) / 1000} seconds.`);
+};
+
+main().catch(err => console.error(err));
