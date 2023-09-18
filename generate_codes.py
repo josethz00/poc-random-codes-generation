c338d520016dbba8ef2c1c28019c6ff80b12da4c
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-09-17 21:38:34 -0300

    feat: algorithm to gnerate sequential codes

--- generate_codes.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generate_code():
-    number_part = f"{random.randint(0, 9999):04}"
-    
-    letters_part = ''.join(random.choices(string.ascii_uppercase, k=3))
-    
-    return number_part + letters_part
--- javascript/generate-code.ts
+++ javascript/generate-code.ts
@@ -1,6 +1,6 @@
 import { getRndInteger } from "./get-random-in";
 
-export function generateCode(): string {
+export function generateRandomCode(): string {
     const numbersPart = getRndInteger(0, 9999).toString().padStart(4, '0');
     let lettersPart = '';
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -11,3 +11,43 @@ export function generateCode(): string {
 
     return numbersPart + lettersPart;
 }
+
+export function generateSequentialCodes(nOfCodes: number = 100): string[] {
+    const codes: string[] = [];
+    let nextNumber = 0;
+    const nextLetters = ['A'.charCodeAt(0), 'A'.charCodeAt(0), 'A'.charCodeAt(0)]
+    let nextNumberPart = '0000';
+    let nextLettersPart = 'AAA';
+    
+    for (let i = 0; i < nOfCodes; i++) {
+        codes.push(`${nextNumberPart}${nextLettersPart}`);
+        nextNumberPart = (nextNumber + 1).toString().padStart(4, '0');
+        
+        nextLetters[2]++;  // increment the last letter
+
+        // check if the last letter is bigger than 'Z' in the ASCII table
+        if (nextLetters[2] > 90) {
+            nextLetters[2] = 65;  // reset to 'A'
+            nextLetters[1]++;  // increment the 2nd letter
+
+            // check if the second letter is bigger than 'Z' in the ASCII table
+            if (nextLetters[1] > 90) {
+                nextLetters[1] = 65;  // reset to 'A'
+                nextLetters[0]++;    // increment the 1st letter
+
+                // if all letters are 'Z', then reset them all to 'A'
+                if (nextLetters[0] > 90) {
+                    nextLetters[0] = 65;
+                    nextLetters[1] = 65;
+                    nextLetters[2] = 65;
+                }
+            }
+        }
+
+        nextLettersPart = String.fromCharCode(...nextLetters);
+    }
+
+    return codes;
+}
+
+console.log(generateSequentialCodes())
